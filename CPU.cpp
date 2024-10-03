92809c1a057b05b33341a7da9ee38988c4b08d37
Adarsh Dhakar <adarshdhakar277@gmail.com>
2024-10-03 23:49:09 +0530

    started

---
+++ CPU.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<string> assemblyCode = {
+        "add x2, x1, x2",
+        "damn:",
+        "addi x5, x6, 1",
+        "hello:",
+        "lui x3, 50",
+        "sw x4, 10(x3)",
+        "jal x7, hello",
+        "beq x8, x9, damn"
+    };
+
+    
+}
