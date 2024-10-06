ce7c335c133f34620aca7deb5fc0d77526f4dadb
Adarsh Dhakar <adarshdhakar277@gmail.com>
2024-10-06 18:40:56 +0530

    without hazards

--- CPU_Design.cpp
+++ CPU_Design.cpp
@@ -105,12 +105,20 @@ public:
         public:
             int RegRead, RegWrite, ALUSrc, ALUOp, Branch, Jump, MemRead, MemWrite, Mem2Reg;
         public:
-            void copyCW(IDEX idex){
-
+            void copyCW(IDEX &idex){
+                this->RegRead = idex.cw.RegRead;
+                this->RegWrite = idex.cw.RegWrite;
+                this->ALUSrc = idex.cw.ALUSrc;
+                this->ALUOp = idex.cw.ALUOp;
+                this->Branch = idex.cw.Branch;
+                this->Jump = idex.cw.Jump;
+                this->MemRead = idex.cw.MemRead;
+                this->MemWrite = idex.cw.MemWrite;
+                this->Mem2Reg = idex.cw.Mem2Reg;
             }
     };
 public:
-    string rs2, rdl;
+    string rdl;
     int ALUOUT;
     CW cw;
 };
@@ -122,7 +130,15 @@ public:
             int RegRead, RegWrite, ALUSrc, ALUOp, Branch, Jump, MemRead, MemWrite, Mem2Reg;
         public:
             void copyCW(EXMO exmo){
-
+                this->RegRead = exmo.cw.RegRead;
+                this->RegWrite = exmo.cw.RegWrite;
+                this->ALUSrc = exmo.cw.ALUSrc;
+                this->ALUOp = exmo.cw.ALUOp;
+                this->Branch = exmo.cw.Branch;
+                this->Jump = exmo.cw.Jump;
+                this->MemRead = exmo.cw.MemRead;
+                this->MemWrite = exmo.cw.MemWrite;
+                this->Mem2Reg = exmo.cw.Mem2Reg;
             }
     };
 public:
@@ -152,7 +168,8 @@ int to_int(string str){
 }
 
 string to_str(int num){
-    bitset<5> binary(num);
+    cout << num << endl;
+    bitset<32> binary(num);
     return binary.to_string();
 }
 
@@ -184,28 +201,111 @@ void ID(IDEX &idex, IFID &ifid){
     idex.rdl = ir.substr(20, 5);
     idex.cw.controller(ir.substr(25, 7));
 
+    if(idex.cw.RegRead){
+        cout << "ir.substr(12, 5)" << " " << ir.substr(12, 5) << endl;
+        cout << "GPR[to_int(ir.substr(12, 5))]" << " " << GPR[to_int(ir.substr(12, 5))] << endl;
+        idex.rs1 = to_str(GPR[to_int(ir.substr(12, 5))]);
+    }
     if(idex.cw.ALUSrc){
         if(idex.cw.RegRead){
+            cout << "ir.substr(0, 12)" << " " << ir.substr(0, 12) << endl;
             idex.rs2 = ir.substr(0, 12);
         }
     }
     else {
         if(idex.cw.RegRead){
-            idex.rs2 = to_str(GPR[to_int(ir.substr(8, 5))]);
+            cout << "ir.substr(7, 5)" << " " << ir.substr(7, 5) << endl;
+            cout << "GPR[to_int(ir.substr(7, 5))]" << " " << GPR[to_int(ir.substr(7, 5))] << endl;
+            idex.rs2 = to_str(GPR[to_int(ir.substr(7, 5))]);
         }
     }
 }
 
-int ALUControl(int ALUOp, string func){
+string ALUControl(int ALUOp, string func) {
+    string ALUSelect;
+    string opcode = "0000000";
+    
+    switch (ALUOp) {
+        case 0:  // Load/Store (for I-type, S-type)
+            ALUSelect = "0010";  // Perform ADD for address calculation
+            break;
+        case 1:  // Branch (for B-type)
+            if (func == "000") {  // beq
+                ALUSelect = "0110";  // Perform SUB for comparison
+            } else if (func == "001") {  // bne
+                ALUSelect = "0110";  // SUB for comparison
+            } else {
+                ALUSelect = "1111";  // Undefined
+            }
+            break;
+        case 10:  // R-type instructions
+            if (func == "000") {  // ADD
+                ALUSelect = opcode == "0000000" ? "0010" : "0110";  // ADD or SUB
+            } else if (func == "001") {  // SLL
+                ALUSelect = "0011";  // Shift left
+            } else if (func == "111") {  // AND
+                ALUSelect = "0000";
+            } else if (func == "110") {  // OR
+                ALUSelect = "0001";
+            } else {
+                ALUSelect = "1111";  // Undefined
+            }
+            break;
+        case 11:  // I-type instructions
+            if (func == "000") {  // ADDI
+                ALUSelect = "0010";  // ADD immediate
+            } else if (func == "010") {  // SLTI
+                ALUSelect = "0111";  // Set less than immediate
+            } else if (func == "100") {  // XORI
+                ALUSelect = "0011";  // XOR immediate
+            } else {
+                ALUSelect = "1111";  // Undefined
+            }
+            break;
+        default:
+            ALUSelect = "1111";  // Undefined operation
+            break;
+    }
 
+    return ALUSelect;
 }
 
-int ALU(int ALUSelect, string rs1, string rs2){
+int ALU(string ALUSelect, string rs1, string rs2) {
+    // Convert input strings to integers
+    int operand1 = bitset<32>(rs1).to_ulong();
+    int operand2 = bitset<32>(rs2).to_ulong();
+    int result = 0;
 
+    // Perform the operation based on ALUSelect
+    if (ALUSelect == "0000") {  // AND
+        result = operand1 & operand2;
+    } else if (ALUSelect == "0001") {  // OR
+        result = operand1 | operand2;
+    } else if (ALUSelect == "0010") {  // ADD
+        result = operand1 + operand2;
+    } else if (ALUSelect == "0110") {  // SUB
+        result = operand1 - operand2;
+    } else if (ALUSelect == "0011") {  // SLL
+        result = operand1 << (operand2 & 0x1F);  // Logical Shift Left
+    } else if (ALUSelect == "0100") {  // SRL
+        result = operand1 >> (operand2 & 0x1F);  // Logical Shift Right
+    } else if (ALUSelect == "0101") {  // SRA
+        result = operand1 >> (operand2 & 0x1F);  // Arithmetic Shift Right
+    } else if (ALUSelect == "0111") {  // SLT (Set Less Than)
+        result = (operand1 < operand2) ? 1 : 0;
+    } else if (ALUSelect == "1000") {  // SLTU (Set Less Than Unsigned)
+        result = ((unsigned int)operand1 < (unsigned int)operand2) ? 1 : 0;
+    } else if (ALUSelect == "1111") {  // Undefined operation
+        std::cerr << "Error: Undefined ALU operation." << std::endl;
+        return -1;  // Return -1 to indicate an error
+    }
+
+    return result;  // Return the result of the ALU operation
 }
 
 void IE(EXMO &exmo, IDEX &idex, IFID &ifid){
-    int ALUSelect = ALUControl(idex.cw.ALUOp, idex.func);
+    string ALUSelect = ALUControl(idex.cw.ALUOp, idex.func);
+    cout << "ALUSelect " << ALUSelect << endl;
     exmo.ALUOUT = ALU(ALUSelect, idex.rs1, idex.rs2);
     int ALUZeroFlag = (idex.rs1 == idex.rs2);
     exmo.cw.copyCW(idex);
@@ -218,18 +318,22 @@ void IE(EXMO &exmo, IDEX &idex, IFID &ifid){
     if(idex.cw.Jump){
         PC = idex.JPC;
     }
+    exmo.rdl = idex.rdl;
 }
 
 void MA(MOWB &mowb, EXMO &exmo, IDEX &idex){
     if(exmo.cw.MemWrite){
         DM[exmo.ALUOUT] = to_int(idex.rs2);
+        cout << "DM[exmo.ALUOUT] " << DM[exmo.ALUOUT] << endl;
     }
     if(exmo.cw.MemRead){
         mowb.LDOUT = DM[exmo.ALUOUT];
+        cout << "DM[exmo.ALUOUT] " << DM[exmo.ALUOUT] << endl;
     }
 
     mowb.ALUOUT = exmo.ALUOUT;
     mowb.cw.copyCW(exmo);
+    mowb.rdl = exmo.rdl;
 }
 
 void RW(MOWB &mowb){
@@ -256,7 +360,7 @@ int main () {
 
     IM = machineCode;
     PC = 0;
-    
+    initControlUnit();
     // while(1) {
     //     RW(mowb);
     //     MA(mowb, exmo, idex);
@@ -290,10 +394,30 @@ int main () {
     cout << "rs2 " << idex.rs2 << endl;
     cout << "JPC " << idex.JPC << endl;
     cout << "DPC " << idex.DPC << endl;
+    cout << endl;
+    cout << "RegRead " << idex.cw.RegRead << endl;
+    cout << "RegWrite " << idex.cw.RegWrite << endl;
+    cout << "ALUSrc " << idex.cw.ALUSrc << endl;
+    cout << "ALUOp " << idex.cw.ALUOp  << endl;
+    cout << "Branch " << idex.cw.Branch << endl;
+    cout << "Jump " << idex.cw.Jump << endl;
+    cout << "MemRead " << idex.cw.MemRead << endl;
+    cout << "MemWrite " << idex.cw.MemWrite << endl;
+    cout << "Mem2Reg " << idex.cw.Mem2Reg << endl;
+    
+    IE(exmo, idex, ifid);
+    cout << endl;
+    cout << "ALUOUT " << exmo.ALUOUT << endl;
 
-    // IE(exmo, idex, ifid);
-    // MA(mowb, exmo, idex);    
-    // RW(mowb);
+    MA(mowb, exmo, idex);
+    cout << endl;
+    cout << "mowb.ALUOUT " << mowb.ALUOUT << endl;
+    cout << "mowb.LDOUT " << mowb.LDOUT << endl;
+    cout << "mowb.rdl " << mowb.rdl << endl;  
+
+    RW(mowb);
+    cout << endl;
+    cout << "GPR[to_int(mowb.rdl)]" << GPR[to_int(mowb.rdl)] << endl;
 
     return 0;
 }
